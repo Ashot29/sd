c64454250c9a1f62291be33ef483ee32abb087a7
Ashot <sargsyan.ashot19@gmail.com>
2021-10-06 08:43:21 +0400

    userModalActionCreator done

--- src/stateManagement/actions/userModalActionCreator.tsx
+++ src/stateManagement/actions/userModalActionCreator.tsx
@@ -1,3 +1,4 @@
+import { IUser } from "../../services/user.service"
 import { CLOSE_USER_MODAL, OPEN_USER_MODAL } from "../reducers/userModalReducer"
 
 export const closeUserModal = () => {
@@ -6,7 +7,7 @@ export const closeUserModal = () => {
     }
 }
 
-export const openUserModal = (userInfo) => {
+export const openUserModal = (userInfo: IUser) => {
     return {
         type: OPEN_USER_MODAL,
         payload: userInfo
